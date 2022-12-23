a14f4bd25af2e4f0f2cd6c2f18dcd03be32207ee
Katrolli <ennio_cano@live.com>
2022-12-23 04:43:40 -0800

    testing 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,29 @@
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *tmp;
+
+    if (*list == NULL)
+        return;
+    tmp = (*list)->next;
+    while (tmp)
+    {
+        while (tmp->prev != NULL && tmp->prev->n > tmp->n)
+        {
+            tmp->prev->next = tmp->next;
+            if (tmp->next != NULL)
+                tmp->next->prev = tmp->prev;
+            tmp->next = tmp->prev;
+            tmp->prev = tmp->next->prev;
+            tmp->next->prev = tmp;
+            if (tmp->prev != NULL)
+            {
+
+                tmp->prev->next = tmp;
+            }
+            else
+                *list = tmp;
+            print_list(*list);
+        }
+        tmp = tmp->next;
+    }
+}
