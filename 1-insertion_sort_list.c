604277505149f4c7ddb9fec18fd37801c8f4ff9c
Katrolli <ennio_cano@live.com>
2023-01-02 12:08:40 -0800

    fixing betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,33 @@
 #include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a list
+ * @list: list to be sorted
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *tmp;
+	listint_t *tmp;
 
-    if (list == NULL)
-        return;
-    tmp = (*list)->next;
-    while (tmp)
-    {
-        while (tmp->prev != NULL && tmp->prev->n > tmp->n)
-        {
-            tmp->prev->next = tmp->next;
-            if (tmp->next != NULL)
-                tmp->next->prev = tmp->prev;
-            tmp->next = tmp->prev;
-            tmp->prev = tmp->next->prev;
-            tmp->next->prev = tmp;
-            if (tmp->prev != NULL)
-            {
-
-                tmp->prev->next = tmp;
-            }
-            else
-                *list = tmp;
-            print_list(*list);
-        }
-        tmp = tmp->next;
-    }
+	if (list == NULL)
+		return;
+	tmp = (*list)->next;
+	while (tmp)
+	{
+		while (tmp->prev != NULL && tmp->prev->n > tmp->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->next->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev != NULL)
+			{
+				tmp->prev->next = tmp;
+			}
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+		tmp = tmp->next;
+	}
 }
